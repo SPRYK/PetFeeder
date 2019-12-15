44aad3c9b8aa828ae933170341f506e955c7dc25
เศรษฐ์ ภูภักดิ์ <Seth214@hotmail.com>
2019-12-15 16:59:19 +0700

    Web and NodeMCU Code

--- EbProject/EbProject.ino
+++ EbProject/EbProject.ino
@@ -12,20 +12,50 @@ const char* password = "Your WiFi password";
 #define ALIAS   "NodeMCU1"
 #define TargetWeb "HTML_web"
 
-#define D4 2   // TXD1
-#define DHTPIN D4     // what digital pin we're connected to
-#define DHTTYPE DHT11   // DHT 11
+//#define D4 2   // TXD1
+//#define DHTPIN D4     // what digital pin we're connected to
+//#define DHTTYPE DHT11   // DHT 11
 
-DHT dht(DHTPIN, DHTTYPE);
+//DHT dht(DHTPIN, DHTTYPE);
 
 WiFiClient client;
 MicroGear microgear(client);
 
 void onMsghandler(char *topic, uint8_t* msg, unsigned int msglen) 
 {
-    Serial.print("Incoming message --> ");
-    msg[msglen] = '\0';
-    Serial.println((char *)msg);
+  Serial.print("Incoming message --> ");
+  Serial.print(topic);
+  Serial.print(" : ");
+  int check;
+  char strState[msglen];
+  for (int i = 0; i < msglen; i++) 
+  { 
+    if ((char)msg[i] == '/') {
+      check = i;  
+    }
+    strState[i] = (char)msg[i];
+    Serial.print((char)msg[i]);
+  }
+  Serial.println();
+  
+  String stateStr = String(strState).substring(0, msglen); 
+  
+  if(stateStr == "ON") 
+  {
+    digitalWrite(ledPin, LOW);
+    microgear.chat(TargetWeb, "ON");
+  } 
+  else if (stateStr == "OFF") 
+  {
+    digitalWrite(ledPin, HIGH);
+    microgear.chat(TargetWeb, "OFF");
+  }
+  else {
+    String newAmount = stateStr.substring(0,check);
+    String newEmail = stateStr.substring(check+1,msglen)
+    // TODO Set new parameter
+  
+  }
 }
 
 
@@ -41,7 +71,7 @@ void setup()
     microgear.on(MESSAGE,onMsghandler);
     microgear.on(CONNECTED,onConnected);
 
-    dht.begin();
+    //dht.begin();
     Serial.begin(115200);
     Serial.println("Starting...");
 
@@ -67,13 +97,17 @@ void loop()
        microgear.loop();
        Serial.println("connected");
 
-       float Humidity = dht.readHumidity();
-       float Temp = dht.readTemperature();  // Read temperature as Celsius (the default)
-       String data = "/" + String(Humidity) + "/" + String(Temp);
-       char msg[128];
-       data.toCharArray(msg,data.length());
-       Serial.println(msg);    
+       //float Humidity = dht.readHumidity();
+       //float Temp = dht.readTemperature();  // Read temperature as Celsius (the default)
+       //String data = "/" + String(Humidity) + "/" + String(Temp);
+       //char msg[128];
+       //data.toCharArray(msg,data.length());
+      // Serial.println(msg);    
 
+      //TODO get input to sent by this format
+      
+      String data = "/" +String(currentUltra)
+                      + "/" + String(minUltra) "/" + String(status);
        microgear.chat(TargetWeb , msg);
     }
    else 
